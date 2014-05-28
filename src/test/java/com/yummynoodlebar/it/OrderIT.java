a9d234df2d7701d66138d33978ec424833a748ea
trung <trung@mdkt.org>
2014-05-29 07:42:16 +0800

    added new test case

--- src/test/java/com/yummynoodlebar/it/OrderIT.java
+++ src/test/java/com/yummynoodlebar/it/OrderIT.java
@@ -3,6 +3,9 @@
  */
 package com.yummynoodlebar.it;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.fail;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,8 +23,11 @@ import org.mdkt.datawiz.annotation.TadahDataSpec;
 import org.mdkt.datawiz.annotation.TadahDataSpecList;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.codec.Base64;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.yummynoodlebar.rest.domain.Order;
@@ -66,6 +72,25 @@ public class OrderIT {
 
 		Order actualOrder = responseEntity.getBody();
 		Assert.assertEquals("Number of items", expectedOrder.size(), actualOrder.getItems().size());
+	}
+
+	@TadahDataSpec(value="User <span tadah:var='auth'>letsnosh:BAD_PWD</span> logged in", handler=OrderDataWizHandler.class)
+	@Test
+	public void notAuthenticatedUserCreateOrders() {
+		String auth = DataWizEngine.instance().getContextValue("auth", String.class);
+		HttpEntity<String> requestEntity = new HttpEntity<String>(
+		        OrderDataWizHandler.toOrderJSON(new HashMap<String, Integer>()), getHeaders(auth));
+
+		RestTemplate template = new RestTemplate();
+		try {
+			ResponseEntity<Order> entity = template.postForEntity(
+	          "http://localhost:8080/tadah-sample/aggregators/orders",
+	          requestEntity, Order.class);
+
+			fail("Request Passed incorrectly with status " + entity.getStatusCode());
+	    } catch (HttpClientErrorException ex) {
+	    	assertEquals(HttpStatus.UNAUTHORIZED, ex.getStatusCode());
+	    }
 	}
 
 	static HttpHeaders getHeaders(String auth) {
